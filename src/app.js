64321aa1dcaa39aafa9abca683389b7656aa7260
Yogesh Priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-07-30 11:36:59 +0530

    new

--- src/app.js
+++ src/app.js
@@ -11,6 +11,19 @@ const app = express();
 
 const server = createServer(app);
 
+
+// OR dynamically allow origin:
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:1000");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
+
+
+
+
 app.use(cors({  origin: 'http://localhost:1000', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
